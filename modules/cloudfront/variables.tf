b1a27dd353de0b13e074fe9aaaafc62714ab505b
femmyte <sanyaoluadefemi@gmail.com>
2023-10-27 07:29:57 +0100

    Fixed some errors

--- modules/cloudfront/variables.tf
+++ modules/cloudfront/variables.tf
@@ -1,3 +1,15 @@
 variable "s3_bucket" {
   type = string
+}# variable "frontend_s3_bucket" {
+#   type = string
+# # default = "pulse-react-bucket"
+# }
+variable "bucket_regional_domain_name" {
+  type = string
+}
+variable "bucket_id" {
+  type = string
+}
+variable "bucket_arn" {
+  type = string
 }
